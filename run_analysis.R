532b09d16c4390da9264d2ac6612126e53f80a05
Alex Borges <elfatherbrown@gmail.com>
2019-06-05 00:35:15 -0600

    All done

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,37 @@
-# Checks for existance of the data directory,
-# outputs instructions otherwise
-library("here")
-library("readr")
-library("stringr")
-library("dplyr")
+
+# Got this handy thing from:
+# http://www.salemmarafi.com/code/install-r-package-automatically/
+# If a package is installed, it will require it. If not, 
+# it will install, then require. Nice!
+usePackage <- function(p) 
+{
+  if (!is.element(p, installed.packages()[,1]))
+    install.packages(p, dep = TRUE)
+  require(p, character.only = TRUE)
+}
+#install.packages(c("readr","stringr","tidyr","dplyr","janitor"),dependencies = TRUE)
+usePackage("readr")
+usePackage("stringr")
+usePackage("tidyr")
+usePackage("dplyr")
+usePackage("janitor")
+#####
+#This script should comply with the follwing:
+#####
+# 1) Merges the training and the test sets to create one data set.
+# 2) Extracts only the measurements on the mean and standard deviation
+#   for each measurement.
+# 3) Uses descriptive activity names to name the activities in the 
+#   data set
+# 4) Appropriately labels the data set with descriptive variable names.
+# 5) From the data set in step 4, creates a second, independent tidy 
+# data set with the average of each variable for each activity and each subject.
+
 
 #This variables configures the name of the directory of the unzipped dataset
 DATADIR = "UCI HAR Dataset"
 
-#We check if the directory is in the current working directory
+#We check if the data directory is in the current working directory
 checkData <- function() {
   if (DATADIR %in% dir()) {
     print("UCI HAR Dataset directory found")
@@ -26,6 +49,38 @@ checkData <- function() {
 ####
 # Main functions
 ###
+
+#1.- Merge The Datasets
+#3.- Uses descriprive activity_names (featureVector provides this)
+mergeTheDatasets <- function() {
+  return(bind_rows(featureVector("test"),featureVector("train")))
+}
+
+#2.- Extract only mean and standard deviation meassurments
+meanAndSv<-function(m=FALSE) {
+  if(is.data.frame(m)){
+    s<-m
+  }
+  else
+  {
+    s<-mergeTheDatasets()
+    
+  }
+  return(s%>%select(1:5,contains("mean"),contains("std")))
+}
+
+#5.- Get a dataset with the average of each activity and each subject
+byActivitySubject <- function (m=FALSE) {
+  if(is.data.frame(m)){
+    s<-m
+  }
+  else
+  {
+    s<-meanAndSv()
+  }
+  return(s%>%group_by(activity_name,subject) %>% summarise_at(.vars = -(1:5),.funs=mean))
+}
+
 
 #The documentation specifies:
 # For each record it is provided:
@@ -51,7 +106,7 @@ featureVector <- function (dname) {
   v <- featureVarnames()
   v <- as.vector(v$name)
   colnames(t) <- v
-  t <- as_tibble(t)
+  t <- as_tibble(t,.name_repair="universal")
   #Add the subjects, they are listed in the same order of the main observations
   #file
   s <- subjects(dname)
@@ -61,8 +116,10 @@ featureVector <- function (dname) {
   t$activity_name <- a$activity_name
   t$obs_id <- seq_along(t$activity_id)
   #Return with the subject as first column, everything else at last
-  t<- t %>% select(obs_id, subject, activity_id, activity_name, everything())
-  return(t)
+  t$set_name<-dname
+  t<- t %>% select(set_name,obs_id, subject, activity_id, activity_name, everything())
+  
+  return(t%>%clean_names())
 }
 
 #signalVector will return the values, one per column, for each file in the
@@ -206,5 +263,50 @@ featureVarnames <- function () {
                sep = " ") %>% select(pos = V1, name = V2)
 }
 
-checkData()
+########
+# The program itself
+########
 
+#We shall stop if the checkData function does not return TRUE
+stopifnot(checkData())
+print("This script will comply with a set of things")
+print("1.- Merges the training and the test sets to create one data set.")
+print("This is done by the mergeTheDatasets function which I will call now")
+print("Please ignore the New names warnings. They are ok")
+print("")
+print("It can take a while...")
+merged_ds<-mergeTheDatasets()
+print("Examine the merged_ds variable at the end of this run...")
+print("View(merged_ds) will show the full thing. ")
+print("But here is a sample of 10 rows")
+print(sample_n(merged_ds,10))
+print("Scroll up to see the sample")
+print("Remember you can see the whole thing in the merged_ds variable")
+print("2.- Extracts only the measurements on the mean and standard deviation for each measurement.")
+print("This is accomplished by my meanAndSv function")
+msv_ds<-meanAndSv(merged_ds)
+print("Check the sample now:")
+print(sample_n(msv_ds,10))
+print("Scroll up to see the sample")
+print("Remember you can see the whole thing in the msv_ds variable")
+
+print("3.- Uses descriptive activity names to name the activities in the data set")
+print("You can see the activity_name variable in either dataset")
+print("has descriptive activity names. I extracted them from msv_ds for you:")
+print(unique(msv_ds$activity_name))
+
+print("4.- Appropriately labels the data set with descriptive variable names.")
+print("As I understand it, no camel case, no funny characters, and descriptive")
+print("info in each variable name should be provided. You can see my variables")
+print("comply with this")
+print(head(names(msv_ds),10))
+print("5.- From the data set in step 4, creates a second, independent tidy")
+print("data set with the average of each variable for each activity and each subject.")
+print("This is accomplished by the byActivitySubject function")
+act_subj<-byActivitySubject(msv_ds)
+print("You can see the result in the act_subj dataframe")
+print("here is a sample")
+print(head(act_subj,10))
+
+print("Hi, I executed in four steps all of the required stuff")
+print("Scroll up to see what I did")
